e6d2ea88a517b8b1d02aa446993deb2c4f13a1bc
Kimia <kimia_moftakhari@hotmail.com>
2022-03-15 21:36:47 +1100

    Edited_Info

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { MichaelNguyen } from "./components/MichaelNguyen/MichaelNguyen";
 import { EthanMoliver } from "./components/EthanMoliver/EthanMoliver";
+import { KimiaMoftakhari } from "./components/kimiaMoftakhari/kimiaMoftakhari";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <p>Name | Mantel Brand | Favourite Food | Last show watched</p>
         <MichaelNguyen />
         <EthanMoliver />
+          <KimiaMoftakhari />
       </header>
     </div>
   );
---
+++ src/components/KimiaMoftakhari/kimiaMoftakhari.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const KimiaMoftakhari = () => {
+  return <p>Kimia Moftakhari | Eliiza | Spicy Food | CoCo</p>;
+};
