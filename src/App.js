476721c13df2bf263eca3bc8b483b4c1dc0c1494
Ruhaab Sheikh <ruhaab@ruhaabs-mbp.home>
2022-03-15 14:47:05 +1100

    Added Ruhaab's Line

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { MichaelNguyen } from "./components/MichaelNguyen/MichaelNguyen";
 import { EthanMoliver } from "./components/EthanMoliver/EthanMoliver";
+import { RuhaabSheikh } from "./components/RuhaabSheikh/RuhaabSheikh";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <p>Name | Mantel Brand | Favourite Food | Last show watched</p>
         <MichaelNguyen />
         <EthanMoliver />
+        <RuhaabSheikh />
       </header>
     </div>
   );
---
+++ src/components/RuhaabSheikh/RuhaabSheikh.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const RuhaabSheikh = () => {
+  return (
+    <p>Ruhaab Sheikh | CMD | Biryani | Formula 1: Drive to Survive</p>
+  )
+}
