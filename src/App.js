14218353652796451dd0c61cdc2b300932c99dde
Olly <ollywilson98@gmail.com>
2022-03-15 14:35:10 +1100

    olly-wilson/Added OllyWilson component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import { MichaelNguyen } from './components/MichaelNguyen/MichaelNguyen';
+import logo from "./logo.svg";
+import "./App.css";
+import { MichaelNguyen } from "./components/MichaelNguyen/MichaelNguyen";
+import { OllyWilson } from "./components/OllyWilson/OllyWilson";
 
 function App() {
   return (
@@ -8,7 +9,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Name | Mantel Brand | Favourite Food | Last show watched</p>
-        <MichaelNguyen /> 
+        <MichaelNguyen />
+        <OllyWilson />
       </header>
     </div>
   );
