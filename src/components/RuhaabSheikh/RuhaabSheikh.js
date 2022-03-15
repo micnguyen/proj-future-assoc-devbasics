ee19575091f45d4047b80480617ec93c04a6eca4
Ruhaab Sheikh <ruhaab@ruhaabs-mbp.home>
2022-03-15 14:35:50 +1100

    Updated Ruhaab's line

--- src/components/RuhaabSheikh/RuhaabSheikh.js
+++ src/components/RuhaabSheikh/RuhaabSheikh.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export const RuhaabSheikh = () => {
   return (
-    <p>Ruhaab Sheikh | CMD | Biryani | South Park</p>
+    <p>Ruhaab Sheikh | CMD | Biryani | Formula 1: Drive to Survive</p>
   )
 }
