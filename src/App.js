43129e3b45f44ceada7c48fd071e22da4cf9379d
KB0982 <kosta.barba@cmd.com.au>
2022-03-15 14:46:05 +1100

    Kosta component added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import { MichaelNguyen } from './components/MichaelNguyen/MichaelNguyen';
+import { KostaBarba } from './components/KostaBarba/KostaBarba';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Name | Mantel Brand | Favourite Food | Last show watched</p>
+        <p>Name | Mantel Brand | Favourite Food | Last show watched</p>z
         <MichaelNguyen /> 
+        <KostaBarba />
       </header>
     </div>
   );
---
+++ src/components/KostaBarba/KostaBarba.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const KostaBarba = () => {
+  return (
+    <p>Kosta Barba| CMD | Pasta | Parks and Rec</p>
+  )
+}
